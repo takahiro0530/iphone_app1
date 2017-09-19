0216c6e6e0098e466c21233cc50c5bccf1b14543
崇裕 今井 <takashihiroshiimai@takashihiroshi-no-MacBook-Air.local>
2017-09-19 22:06:30 +0900

    Initial Commit

---
+++ AlcoholChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlcoholChecker
+//
+//  Created by 崇裕 今井 on 2017/09/19.
+//  Copyright © 2017年 Takahiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
